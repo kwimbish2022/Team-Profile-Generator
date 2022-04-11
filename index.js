c4ee51532d0e16a131805a72f390ee4413bccc15
Nibbler <kwimbish@hotmail.com>
2022-04-10 20:38:46 -0500

    employee info inquirer prompts

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const inquirer = require('inquirer');
 // all employee array
 const allEmployees = [];
 
-// adding a manager to the all employee
+// adding a manager to the all employee array
 const addManager = ( => {
   return inquirer.prompt([
     {
@@ -69,6 +69,7 @@ const addManager = ( => {
       }
     }
   ])
+
   .then(managerInput => {
     const { employeeName, employeeId, emailAddress, officeNumber } = managerInput;
     const manager = new Manager (employeeName, employeeId, emailAddress, officeNumber);
@@ -77,3 +78,113 @@ const addManager = ( => {
     console.log(manager);
   })
 });
+
+// adding employees/interns to the all employee array
+const addEmployee = () => {
+  console.log(`adding employees to our group`);
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'role',
+      message: 'Please select the employees role. (Required)',
+      choices: ['Engineer', 'Intern']
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the employees name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('You must enter the employees name.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'employee id',
+      message: 'Please enter the employees employee Id number. (Required)',
+      validate: idInput => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log('You must enter the employees employee id number.');
+          return false;
+        }
+      } 
+    },
+    {
+      type: 'input',
+      name: 'email address',
+      message: 'Please enter the employees email address. (Required)',
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log('You  must enter the employees email address.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter the employees github username. (Required)',
+      when: (input) => input.role === "Engineer",
+      validate: usernameInput => {
+        if (usernameInput) {
+          return true;
+        } else {
+          console.log('You must enter the employeees github username.')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Please enter the interns school name. (Required)',
+      when: (input) => input.role === "Intern",
+      validate: schoolInput => {
+        if (schoolInput) {
+          return true;
+        } else {
+          console.log('You must enter the interns school name.')
+          return false;
+        }
+      }    
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddEmployee',
+      message: 'Would you like to add another employee?',
+      default: false
+    }  
+  ])
+
+  .then(employeeData => {
+    // the data for all the different employee types
+    let { employeeName, employeeId, emailAddress, role, github, school, confirmAddEmployee } = employeeData;
+    let employee;
+
+    if (role === "Engineer") {
+      employee = new Engineer (employeeName, employeeId, emailAddress, github);
+      console.log(employee);
+    
+    } else if (role === "Intern") {
+      employee = new Intern (employeeName, employeeId, emailAddress, role, school);
+      console.log(employee);
+    }
+
+    allEmployees.push(employee);
+
+    if (confirmAddEmployee) {
+      return addEmployee(allEmployees);
+    } else {
+      return allEmployees;
+    }
+  })
+};
